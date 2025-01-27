8282cba01f181b4ccb425bff8c93e79a36f04f6a
justaweirdo <tube00216@gmail.com>
2025-01-27 17:45:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -309,7 +309,7 @@ app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 app.mount("/js", StaticFiles(directory="./statics/js"), name="static")
 app.mount("/css", StaticFiles(directory="./statics/css"), name="static")
 app.mount("/img", StaticFiles(directory="./statics/img"), name="static")
-app.mount("/genesis", StaticFiles(directory="./blog", html=True), name="static")
+app.mount("/home", StaticFiles(directory="./blog", html=True), name="static")
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 from fastapi.templating import Jinja2Templates
@@ -323,7 +323,7 @@ def home(response: Response, request: Request, yuki: Union[str] = Cookie(None)):
         response.set_cookie("yuki", "True", max_age=60 * 60 * 24 * 7)
         return template("home.html", {"request": request})
     print(checkCookie(yuki))
-    return redirect("/genesis")
+    return redirect("/home")
 
 
 @app.get('/watch', response_class=HTMLResponse)
