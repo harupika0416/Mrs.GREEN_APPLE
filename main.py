c0392e44326c4d3f16cb8864e2f8fdda39f0a8ff
justaweirdo <tube00216@gmail.com>
2025-02-02 05:43:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -430,7 +430,8 @@ def bbs(request: Request, name: Union[str, None] = "", seed:Union[str, None]="",
     if not(checkCookie(yuki)):
         return redirect("/")
     res = HTMLResponse(no_robot_meta_tag + requests.get(f"{url}bbs?name={urllib.parse.quote(name)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}", cookies={"yuki":"True"}).text.replace('AutoLink(xhr.responseText);', 'urlConvertToLink(xhr.responseText);') + getSource('bbs'))
-    return res
+    return template("bbs.html",{"request":request})
+  　return res
 
 @cache(seconds=5)
 def getCachedBBSAPI(verify, channel):
